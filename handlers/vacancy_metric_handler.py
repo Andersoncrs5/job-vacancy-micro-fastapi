ae377a934899078da37f1f8e4cc1bc6b6bd88b48
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-11 18:34:26 -0300

    Impled VacancyMetricHandler

---
+++ handlers/vacancy_metric_handler.py
@@ -0,0 +1,13 @@
+from schemas.event_message_metric_schemas import EventMessageMetric
+from services.provider.vacancy_metric_service_provider import VacancyMetricServiceProvider
+
+
+class VacancyMetricHandler:
+    def __init__(self, event: EventMessageMetric, service: VacancyMetricServiceProvider):
+        self.event = event
+        self.service = service
+
+    async def handle(self):
+
+        metric = await self.service.get_by_id(self.event.metric_id)
+        await self.service.save_metric(metric, self.event)
