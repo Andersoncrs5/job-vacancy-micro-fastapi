9d694dbceea93f9fdaeba8784784f5448d4f1f28
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-12 17:46:59 -0300

    Register method save_metric

--- services/base/user_metric_service_base.py
+++ services/base/user_metric_service_base.py
@@ -1,13 +1,17 @@
 from abc import ABC, abstractmethod
 
 from configs.db.database import UserMetricEntity
-from schemas.event_message_metric_schemas import SumRedEnum
+from schemas.event_message_metric_schemas import SumRedEnum, EventMessageMetric
 
 
 class UserMetricServiceBase(ABC):
 
     @abstractmethod
     async def get_by_id(self, user_id: int) -> UserMetricEntity | None:
+        pass
+
+    @abstractmethod
+    async def save_metric(self, metric: UserMetricEntity, event: EventMessageMetric):
         pass
 
     @abstractmethod
