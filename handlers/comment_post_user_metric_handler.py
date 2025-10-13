5383dd94d894273ce08b69c637a48d753fabc42d
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-13 20:36:10 -0300

    Impled comment_post_user_metric_handler.py

---
+++ handlers/comment_post_user_metric_handler.py
@@ -0,0 +1,16 @@
+import structlog
+
+from schemas.event_message_metric_schemas import EventMessageMetric
+from services.base.comment_post_enterprise_metric_service_provider import CommentPostEnterpriseMetricServiceProvider
+from services.base.comment_post_user_metric_service_provider import CommentPostUserMetricServiceProvider
+
+logger = structlog.get_logger()
+
+class CommentPostUserMetricHandler:
+    def __init__(self, event: EventMessageMetric, service: CommentPostUserMetricServiceProvider):
+        self.event = event
+        self.service = service
+
+    async def handle(self):
+        metric = await self.service.get_by_id(self.event.metric_id)
+        await self.service.save_metric(metric, self.event)
