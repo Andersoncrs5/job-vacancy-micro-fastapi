b780301405106f920f15b23babdea06db88ddebf
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-13 20:39:51 -0300

    Set TemplateManager

---
+++ templates/template_manager.py
@@ -0,0 +1,25 @@
+import os
+from jinja2 import Environment, FileSystemLoader
+from datetime import datetime
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates', 'emails')
+
+jinja_env = Environment(
+    loader=FileSystemLoader(TEMPLATE_DIR),
+    autoescape=True,
+)
+
+class TemplateManager:
+    def render_html(self, template_name: str, data: dict) -> str:
+        try:
+            template_file = f"{template_name}.html"
+            template = jinja_env.get_template(template_file)
+            html_content = template.render(data)
+
+            return html_content
+
+        except FileNotFoundError:
+            raise ValueError(f"Template '{template_file}' não encontrado na pasta {TEMPLATE_DIR}.")
+        except Exception as e:
+            raise RuntimeError(f"Erro ao renderizar template '{template_name}': {e}")
