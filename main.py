9ebba0f9686467bea7acaaedebd16239755943e4
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-13 21:47:29 -0300

    Register consume_send_email

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ from fastapi import FastAPI
 import uvicorn
 
 from consumers.metric_consumer import consume_metric_events
+from consumers.send_email_consumer import consume_send_email
 
 app = FastAPI(title="Metric Consumer Microservice")
 
 @app.on_event("startup")
 async def startup_event():
     asyncio.create_task(consume_metric_events())
+    asyncio.create_task(consume_send_email())
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True)
