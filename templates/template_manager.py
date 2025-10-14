eea8f3d7c123b697f499406c895fd4b5a2961bf2
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-13 21:48:17 -0300

    Improvements in jinja_env

--- templates/template_manager.py
+++ templates/template_manager.py
@@ -3,12 +3,16 @@ from jinja2 import Environment, FileSystemLoader
 from datetime import datetime
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates', 'emails')
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'emails')
 
 jinja_env = Environment(
     loader=FileSystemLoader(TEMPLATE_DIR),
     autoescape=True,
 )
+
+jinja_env.globals.update({
+    'now': datetime.now
+})
 
 class TemplateManager:
     def render_html(self, template_name: str, data: dict) -> str:
