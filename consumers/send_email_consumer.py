4a5f0d8e729c62356e00f03f749e92ae02f5d310
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-13 21:47:59 -0300

    Impled logic to read datas of SEND_EMAIL_TOPIC

--- consumers/send_email_consumer.py
+++ consumers/send_email_consumer.py
@@ -1,20 +1,38 @@
 import structlog
 
+from configs.db.database import AsyncSessionLocal
 from configs.db.kafka import get_kafka_consumer, SEND_EMAIL_TOPIC
+from handlers.email_handler import EmailHandler
+from repositories.provider.user_repository_provider import UserRepositoryProvider
 from schemas.event_message_email import EventMessageEmail
+from services.provider.email_service_provider import EmailServiceProvider
+from services.provider.user_service_provider import UserServiceProvider
+from templates.template_manager import TemplateManager
 
 logger = structlog.get_logger()
 
-async def consume_email():
+async def consume_send_email():
     consumer = await get_kafka_consumer(SEND_EMAIL_TOPIC, group_id="email-service")
 
     try:
         async for msg in consumer:
-            try:
-                event = EventMessageEmail.model_validate_json(msg.value.decode("utf-8"))
-                logger.info(f"Event received: {event.model_dump_json()}")
+            async with AsyncSessionLocal() as db:
+                repository_user = UserRepositoryProvider(db)
+                user_service = UserServiceProvider(repository_user)
 
-            except Exception as e:
-                logger.error("Failed to process event", error=str(e), message_value=msg.value.decode("utf-8"))
+                email_service = EmailServiceProvider()
+
+                template_manager = TemplateManager()
+
+                try:
+                    event = EventMessageEmail.model_validate_json(msg.value.decode("utf-8"))
+                    logger.info(f"Event received: {event.model_dump_json()}")
+
+                    handler = EmailHandler(event, user_service, email_service, template_manager)
+                    await handler.send_email_welcome()
+
+                    await consumer.commit()
+                except Exception as e:
+                    logger.error("Failed to process event", error=str(e), message_value=msg.value.decode("utf-8"))
     finally:
         await consumer.stop()
