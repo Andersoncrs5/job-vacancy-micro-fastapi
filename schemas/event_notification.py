5312992437db5694f1d44b015e872467c82fc244
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-20 20:04:08 -0300

    New event EventNotification

---
+++ schemas/event_notification.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from uuid import UUID
+
+from configs.db.enums import NotificationTypeEnum
+from configs.orjson.orjson_config import ORJSONModel
+
+class EventNotification(ORJSONModel):
+    event_id: UUID
+    event_type: NotificationTypeEnum
+    actor_id: int | None
+    entity_id: int
+    created_at: datetime
+    source_service: str
+    data: dict
+    metadata: dict
